f0986126988f028e6cd6cc65774b824dc8c5e06b
Pixeladed <vietminhle98@gmail.com>
2018-04-12 20:15:06 +0800

    add react-ga

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import GA from 'react-ga';
+
+if (process.env.NODE_ENV === 'production') {
+  GA.initialize('UA-117422701-1', {
+    debug: false,
+    titleCase: false,
+  });
+}
+
+export default class Analytics extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.location.pathname !== this.props.location.pathname) {
+      GA.pageview(nextProps.location.pathname);
+    }
+  }
+  render() {
+    return null;
+  }
+}
