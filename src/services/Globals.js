31548c05409f7e903a6fa76f2530500139a79ef8
Anh <anh@canva.com>
2021-03-01 18:04:00 +1100

    change speaker registration link to be editable

--- src/services/Globals.js
+++ src/services/Globals.js
@@ -1,30 +1,29 @@
-const SPEAKER_REGISTRATION_LINK =
-  "https://docs.google.com/forms/d/e/1FAIpQLSet46XC-6Ysjzm8rEshPxtH-yiPcjvmq08ZU_8d_kXUcW6C9g/viewform";
+const SPEAKER_REGISTRATION_LINK = 't.tedxuwa.com/SpeakerApp';
 
 export const YOUTUBE_LINK =
-  "https://www.youtube.com/playlist?list=PLsRNoUx8w3rPD_QqBt6HCdC_fuxAGgWC2";
+  'https://www.youtube.com/playlist?list=PLsRNoUx8w3rPD_QqBt6HCdC_fuxAGgWC2';
 
 export const SOCIAL_MEDIA = [
   {
-    name: "facebook",
-    icon: "facebook",
-    link: "https://www.facebook.com/pg/TEDxUWA/"
+    name: 'facebook',
+    icon: 'facebook',
+    link: 'https://www.facebook.com/pg/TEDxUWA/',
   },
   {
-    name: "instagram",
-    icon: "instagram",
-    link: "https://www.instagram.com/tedxuwa/"
+    name: 'instagram',
+    icon: 'instagram',
+    link: 'https://www.instagram.com/tedxuwa/',
   },
   {
-    name: "linkedin",
-    icon: "linkedin",
-    link: "https://www.linkedin.com/company/tedxuwa/"
+    name: 'linkedin',
+    icon: 'linkedin',
+    link: 'https://www.linkedin.com/company/tedxuwa/',
   },
   {
-    name: "flickr",
-    icon: "flickr",
-    link: "https://www.flickr.com/photos/146823452@N07/"
-  }
+    name: 'flickr',
+    icon: 'flickr',
+    link: 'https://www.flickr.com/photos/146823452@N07/',
+  },
 ];
 
 export { SPEAKER_REGISTRATION_LINK };
