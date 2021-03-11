6ee4fbb47e34828810758d56c3d137a192eceaea
Anh <anh@canva.com>
2021-03-11 21:25:11 +1100

    use reach data from database

--- src/components/Reach.jsx
+++ src/components/Reach.jsx
@@ -1,39 +1,54 @@
-import React from "react";
+import React from 'react';
+import API from '../services/Api';
 
 const Reach = () => {
+  const [reach, setReach] = React.useState({});
+
+  const fetchReach = () => {
+    API.GET('reach')
+      .then((data) => {
+        setReach(data);
+      })
+      .catch((err) => {
+        console.error(`Something went wrong: ${err.message}`);
+      });
+  };
+
+  React.useEffect(() => {
+    fetchReach();
+  });
+
   return (
-    <div className="pt-5 bg-white">
-      <div className="container pb-5">
-        <h4 className="font-weight-bold">Our Audience</h4>
-        <div className="row mt-0 mt-md-4">
-          <div className="col-md-4 mt-4 mt-md-0">
-            <strong className="display-4 font-weight-bold text-primary">
-              3k
+    <div className='pt-5 bg-white'>
+      <div className='container pb-5'>
+        <h4 className='font-weight-bold'>Our Audience</h4>
+        <div className='row mt-0 mt-md-4'>
+          <div className='col-md-4 mt-4 mt-md-0'>
+            <strong className='display-4 font-weight-bold text-primary'>
+              {reach['followers']}
             </strong>
             <p>
               <strong>Followers</strong>
               <br />
-              <span className="text-muted">on our social media platforms.</span>
+              <span className='text-muted'>on our social media platforms.</span>
             </p>
           </div>
-          <div className="col-md-4 mt-4 mt-md-0">
-            <strong className="display-4 font-weight-bold text-primary">
-              80
-            </strong>
+          <div className='col-md-4 mt-4 mt-md-0'>
+            <strong className='display-4 font-weight-bold text-primary'>{reach['speakers']}</strong>
             <p>
               <strong>Speakers</strong>
               <br />
-              <span className="text-muted">at our events to date.</span>
+              <span className='text-muted'>at our events to date.</span>
             </p>
           </div>
-          <div className="col-md-4 mt-4 mt-md-0">
-            <strong className="display-4 font-weight-bold text-primary">
-              854
+          <div className='col-md-4 mt-4 mt-md-0'>
+            <strong className='display-4 font-weight-bold text-primary'>
+              {reach['subscribers']}
             </strong>
             <p>
               <strong>Subscribers</strong>
               <br />
-              <span className="text-muted">on our mailing list.</span>
+              <span className='text-muted'>on our mailing list.</span>
             </p>
           </div>
         </div>
--- src/pages/SponsorsPage.jsx
+++ src/pages/SponsorsPage.jsx
@@ -1,74 +1,75 @@
-import React, { Component } from 'react'
-import PageHeader from '../components/PageHeader'
-import ContactForm from '../components/ContactForm'
-import SponsorCard from '../components/SponsorCard'
-import Reach from '../components/Reach'
-import banner from '../assets/slogan.jpg'
-import API from '../services/Api'
-import { PROSPECTUS_LINK } from "../services/Globals";
-import '../css/SponsorsPage.css'
+import React, { Component } from 'react';
+import PageHeader from '../components/PageHeader';
+import ContactForm from '../components/ContactForm';
+import SponsorCard from '../components/SponsorCard';
+import Reach from '../components/Reach';
+import banner from '../assets/slogan.jpg';
+import API from '../services/Api';
+import { PROSPECTUS_LINK } from '../services/Globals';
+import '../css/SponsorsPage.css';
 
 export default class SponsorsPage extends Component {
   state = {
     sponsorsExpanded: false,
-    sponsors: []
-  }
+    sponsors: [],
+  };
   componentDidMount() {
-    document.title = 'TEDxUWA | Work with us'
-    this.fetchSponsors()
+    document.title = 'TEDxUWA | Work with us';
+    this.fetchSponsors();
   }
   fetchSponsors = () => {
     API.GET('sponsors')
-      .then(data => {
-        const sponsors = this.groupSponsors(data.results)
-        this.setState({ sponsors })
+      .then((data) => {
+        const sponsors = this.groupSponsors(data.results);
+        this.setState({ sponsors });
       })
-      .catch(err => {
-        console.error(`Something went wrong: ${err.message}`)
-      })
-  }
-  groupSponsors = sponsors =>
+      .catch((err) => {
+        console.error(`Something went wrong: ${err.message}`);
+      });
+  };
+
+  groupSponsors = (sponsors) =>
     Object.values(
       sponsors.reduce((groups, sponsor) => {
-        const { tier } = sponsor
-        if (groups[tier]) groups[tier].push(sponsor)
-        else groups[tier] = [sponsor]
-        return groups
+        const { tier } = sponsor;
+        if (groups[tier]) groups[tier].push(sponsor);
+        else groups[tier] = [sponsor];
+        return groups;
       }, {})
-    )
-  expand = () => this.setState({ sponsorsExpanded: true })
+    );
+  expand = () => this.setState({ sponsorsExpanded: true });
   render() {
-    const { sponsors } = this.state
+    const { sponsors } = this.state;
     return (
-      <div className="sponsors page">
-        <PageHeader title="Work With Us" image={banner} />
-        <div className="bg-primary text-white mission-group">
-          <div className="py-5 container">
-            <div className="row">
-              <div className="col-md-6">
-                <h1 className="font-weight-bold mb-3">
+      <div className='sponsors page'>
+        <PageHeader title='Work With Us' image={banner} />
+        <div className='bg-primary text-white mission-group'>
+          <div className='py-5 container'>
+            <div className='row'>
+              <div className='col-md-6'>
+                <h1 className='font-weight-bold mb-3'>
                   Our <br />
                   Mission
                 </h1>
               </div>
-              <div className="col-md-6">
-                <p className="lead mb-3">
-                  TEDxUWA aims to spread good ideas among the UWA community.
-                  Creating a synergy of ideas between different disciplines and
-                  people
+              <div className='col-md-6'>
+                <p className='lead mb-3'>
+                  TEDxUWA aims to spread good ideas among the UWA community. Creating a synergy of
+                  ideas between different disciplines and people
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="container py-5">
-          <h3 className="font-weight-bold mb-4 text-center">Our partners</h3>
-          {sponsors.map(group => (
-            <div className="gold-group row text-center d-flex justify-content-center mb-3">
-              {group.map(sponsor => (
+        <div className='container py-5'>
+          <h3 className='font-weight-bold mb-4 text-center'>Our partners</h3>
+          {sponsors.map((group) => (
+            <div className='gold-group row text-center d-flex justify-content-center mb-3'>
+              {group.map((sponsor) => (
                 <div
-                  className="col-sm-6 col-md-4 mb-3 card border-0 text-center"
-                  key={sponsor.name}>
+                  className='col-sm-6 col-md-4 mb-3 card border-0 text-center'
+                  key={sponsor.name}
+                >
                   <SponsorCard sponsor={sponsor} />
                 </div>
               ))}
@@ -77,32 +78,27 @@ export default class SponsorsPage extends Component {
         </div>
         <hr />
         <Reach />
-        <div className="call-to-action-group bg-light py-4">
-          <div className="container py-4">
-            <div className="row">
-              <div className="col-md-6 mb-4 mb-md-0">
-                <h3 className="font-weight-bold mb-3">
-                  Become a sponsor today!
-                </h3>
-                <p className="lead text-muted">
-                  Contact us to discuss how you can help make a difference with
-                  TEDxUWA. If your business specialises in venue, catering,
-                  marketing, signage, production or merchendise any assistance
-                  would be greatly appreciated.
+        <div className='call-to-action-group bg-light py-4'>
+          <div className='container py-4'>
+            <div className='row'>
+              <div className='col-md-6 mb-4 mb-md-0'>
+                <h3 className='font-weight-bold mb-3'>Become a sponsor today!</h3>
+                <p className='lead text-muted'>
+                  Contact us to discuss how you can help make a difference with TEDxUWA. If your
+                  business specialises in venue, catering, marketing, signage, production or
+                  merchendise any assistance would be greatly appreciated.
                 </p>
                 <a href={PROSPECTUS_LINK}>
-                  <button className="btn btn-dark mt-3">
-                    Download our prospectus
-                  </button>
+                  <button className='btn btn-dark mt-3'>Download our prospectus</button>
                 </a>
               </div>
-              <div className="col-md-6">
+              <div className='col-md-6'>
                 <ContactForm noTitle />
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
