061f2200812941e659b4891d0acabc33eaf70e3b
Anh <anh@canva.com>
2021-06-22 16:59:09 +1000

    use link shortener

--- src/global/constants.js
+++ src/global/constants.js
@@ -1,3 +1,2 @@
 export const slugConfig = { lower: true, remove: /[*+~.()'"!:@]/g }
-export const YOUTUBE_PLAYLIST_URL =
-  'https://www.youtube.com/watch?v=p6780Y7i6zA&list=PLsRNoUx8w3rMFxO2T7p3qwBKXODblEx_l'
+export const YOUTUBE_PLAYLIST_URL ='https://t.tedxuwa.com/watch'
